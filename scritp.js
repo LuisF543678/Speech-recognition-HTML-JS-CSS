f7b9fab0d9313baef905f2bbc09605adffa28f99
Luisf543678 <luisf543678@gmail.com>
2021-05-09 14:20:23 -0500

    starting

---
+++ scritp.js
@@ -0,0 +1,20 @@
+const boton = document.querySelector('#hablar');
+const contend = document.querySelector('#contenido');
+
+const reconocer_voz = window.SpeechRecognition || window.webkitSpeechRecognition;
+const reconocer = new reconocer_voz();
+reconocer.onstart = function () {
+    console.log('El microfono esta activo');
+}
+
+reconocer.onresult = function (event) {
+    //console.log(event);
+    const current = event.resultIndex;
+    const transcripcion = event.results[current][0].transcript;
+    contend.textContent = transcripcion;
+
+}
+
+boton.addEventListener('click', () => {
+    reconocer.start();
+});
